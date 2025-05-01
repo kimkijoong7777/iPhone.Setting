ae66e448dbe772e9f098839dab26dc86bac02fab
kimkijoong7777 <kimkijoong7777@gmail.com>
2025-05-01 12:34:19 +0900

    Update ContentView.swift

--- project1/project1.swiftpm/ContentView.swift
+++ project1/project1.swiftpm/ContentView.swift
@@ -1,8 +1,8 @@
 //
 //  ContentView.swift
-//  ios_setting_clone
+//  project
 //
-//  Created by jsy on 4/24/25.
+//  Created by kimkijoong on 4/24/25.
 //
 
 import SwiftUI
@@ -14,14 +14,13 @@ struct ContentView: View {
         
         NavigationView {
             List {
-                TextField("검색", text: $inputText)  // TextField 만들기
-                    
+                TextField("검색", text: $inputText)
                 Section() {
                     header:do {
                         HStack{
                             Image("apple accunt")
                                 .resizable()
-                                .frame(width: 90, height: 90)
+                                .frame(width: 70, height: 70)
                             VStack() {
                                 HStack{
                                     Text("Apple 계정")
@@ -30,7 +29,7 @@ struct ContentView: View {
                                 }
                                 Text("iCloud 데이터, App Store, Apple 서비스등에 접근하려면 로그인 하십시오.")
                                     .foregroundStyle(.gray)
-                                    .font(.system(size: 13))
+                                    .font(.system(size: 7))
                             }
                             NavigationLink("", destination: Text("Apple 계정"))
                         }
@@ -44,7 +43,7 @@ struct ContentView: View {
                         Image(systemName: "airplane")
                             .padding(.all, 6)
                             .frame(width: 25, height: 25)
-                            .background(.white)
+                            .background(.airplane)
                             .foregroundColor(.white)
                             .cornerRadius(5)
                         
@@ -58,8 +57,10 @@ struct ContentView: View {
                             .foregroundColor(.white)
                             .cornerRadius(5)
                         Text("Wi-Fi")
-                        NavigationLink("", destination: Text("Wi-FI"))
-                    }
+                        Spacer().frame(width: 130)
+                                    Text("us_gbsw_hs_p")
+                            .foregroundColor(.gray)
+                                            }
                     HStack{
                         Image("bluetooth")
                             .padding(.all, 6)
@@ -68,7 +69,9 @@ struct ContentView: View {
                             .foregroundColor(.white)
                             .cornerRadius(5)
                         Text("Bluetooth")
-                        NavigationLink("", destination: Text("Bluetooth"))
+                        Spacer().frame(width: 200)
+                                    Text("켬")
+                            .foregroundColor(.gray)
                     }
                     HStack{
                         Image(systemName: "antenna.radiowaves.left.and.right")
@@ -78,7 +81,6 @@ struct ContentView: View {
                             .background(.green)
                             .cornerRadius(5)
                         Text("셀룰러")
-                        NavigationLink("", destination: Text("셀룰러"))
                     }
                     HStack{
                         Image(systemName: "personalhotspot")
@@ -88,7 +90,6 @@ struct ContentView: View {
                             .background(.green)
                             .cornerRadius(5)
                         Text("개인용 핫스팟")
-                        NavigationLink("", destination: Text("개인용 핫스팟"))
                     }
                     HStack{
                         Image(systemName: "battery.100percent")
@@ -98,7 +99,6 @@ struct ContentView: View {
                             .background(.green)
                             .cornerRadius(5)
                         Text("베터리")
-                        NavigationLink("", destination: Text("베터리"))
                     }
                 }
                 Section() {
@@ -110,7 +110,6 @@ struct ContentView: View {
                             .background(.gray)
                             .cornerRadius(5)
                         Text("일반")
-                        NavigationLink("", destination: Text("일반"))
                     }
                     HStack{
                         Image(systemName: "figure.child.circle")
@@ -120,7 +119,6 @@ struct ContentView: View {
                             .background(.blue)
                             .cornerRadius(5)
                         Text("손쉬운 사용")
-                        NavigationLink("", destination: Text("손쉬운 사용"))
                     }
                     HStack{
                         Image(systemName: "magnifyingglass")
@@ -130,7 +128,6 @@ struct ContentView: View {
                             .background(.gray)
                             .cornerRadius(5)
                         Text("검색")
-                        NavigationLink("", destination: Text("검색"))
                     }
                     HStack{
                         Image(systemName: "iphone.gen1.and.arrow.left")
@@ -140,7 +137,6 @@ struct ContentView: View {
                             .background(.blue)
                             .cornerRadius(5)
                         Text("동작 버튼")
-                        NavigationLink("", destination: Text("동작 버튼"))
                     }
                     HStack{
                         Image(systemName: "sun.max.fill")
@@ -150,7 +146,6 @@ struct ContentView: View {
                             .background(.blue)
                             .cornerRadius(5)
                         Text("디스플레이 및 밝기")
-                        NavigationLink("", destination: Text("디스플레이 및 밝기"))
                     }
                     HStack{
                         Image("wallpaper")
@@ -158,14 +153,12 @@ struct ContentView: View {
                             .frame(width: 25, height: 25)
                             .cornerRadius(5)
                         Text("배경화면")
-                        NavigationLink("", destination: Text("배경화면"))
                     }
                     HStack{
                         Image("standby")
                             .resizable()
                             .frame(width: 25, height: 25)
                         Text("스탠바이")
-                        NavigationLink("", destination: Text("스탠바이"))
                     }
                     HStack{
                         Image(systemName: "switch.2")
@@ -175,7 +168,6 @@ struct ContentView: View {
                             .background(.gray)
                             .cornerRadius(5)
                         Text("제어 센터")
-                        NavigationLink("", destination: Text("제어 센터"))
                     }
                     HStack{
                         Image(systemName: "camera.fill")
@@ -185,7 +177,6 @@ struct ContentView: View {
                             .background(.gray)
                             .cornerRadius(5)
                         Text("카메라")
-                        NavigationLink("", destination: Text("카메라"))
                     }
                     HStack{
                         Image(systemName: "apps.iphone")
@@ -195,14 +186,12 @@ struct ContentView: View {
                             .background(.blue)
                             .cornerRadius(5)
                         Text("홈 화면 및 앱 보관함")
-                        NavigationLink("", destination: Text("홈 화면 및 앱 보관함"))
                     }
                     HStack{
                         Image("apple inteligence")
                             .resizable()
                             .frame(width: 25, height: 25)
                         Text("Apple Intelligence 및 Siri")
-                        NavigationLink("", destination: Text("Apple Intelligence 및 Siri"))
                     }
                     }
                     Section() {
@@ -211,10 +200,9 @@ struct ContentView: View {
                                 .padding(.all, 6)
                                 .frame(width: 25, height: 25)
                                 .foregroundColor(.white)
-                                .background(.blue)
+                                .background(.bell)
                                 .cornerRadius(5)
                             Text("알림")
-                            NavigationLink("", destination: Text("알림"))
                         }
                         HStack{
                             Image(systemName: "speaker.wave.3.fill")
@@ -224,7 +212,6 @@ struct ContentView: View {
                                 .background(.sound)
                                 .cornerRadius(5)
                             Text("사운드 및 햅틱")
-                            NavigationLink("", destination: Text("사운드 및 햅틱"))
                         }
                         HStack{
                             Image(systemName: "moon.fill")
@@ -234,7 +221,6 @@ struct ContentView: View {
                                 .background(.mode)
                                 .cornerRadius(5)
                             Text("집중 모드")
-                            NavigationLink("", destination: Text("집중 모드"))
                         }
                         HStack{
                             Image(systemName: "hourglass")
@@ -244,7 +230,6 @@ struct ContentView: View {
                                 .background(.mode)
                                 .cornerRadius(5)
                             Text("스크린 타임")
-                            NavigationLink("", destination: Text("스크린 타임"))
                         }
                     }
                     Section() {
@@ -255,7 +240,6 @@ struct ContentView: View {
                                 .background(.green)
                                 .cornerRadius(5)
                             Text("Face ID 및 암호")
-                            NavigationLink("", destination: Text("Face ID 및 암호"))
                         }
                         HStack{
                             Image(systemName: "sos")
@@ -265,7 +249,6 @@ struct ContentView: View {
                                 .background(.red)
                                 .cornerRadius(5)
                             Text("긴급 구조 요청")
-                            NavigationLink("", destination: Text("긴급 구조 요청"))
                         }
                         HStack{
                             Image(systemName: "hand.raised.fill")
@@ -275,7 +258,6 @@ struct ContentView: View {
                                 .background(.blue)
                                 .cornerRadius(5)
                             Text("개인정보 및 보안")
-                            NavigationLink("", destination: Text("개인정보 및 보안"))
                         }
                     }
                 Section() {
@@ -284,19 +266,19 @@ struct ContentView: View {
                             .resizable()
                             .frame(width: 25, height: 25)
                         Text("지갑 및 Apple Pay")
-                        NavigationLink("", destination: Text("지갑 및 Apple Pay"))}
+                    }
                     HStack{
                         Image("game center")
                             .resizable()
                             .frame(width: 25, height: 25)
                         Text("Game Center")
-                    NavigationLink("", destination: Text("Game Center"))}
+                    }
                     HStack{
                         Image("Cloud")
                             .resizable()
                             .frame(width: 25, height: 25)
                         Text("iCloud")
-                    NavigationLink("", destination: Text("iCloud"))}
+                        }
                     }
                     Section() {
                         HStack{
@@ -307,7 +289,7 @@ struct ContentView: View {
                                 .background(.mode)
                                 .cornerRadius(5)
                             Text("앱")
-                            NavigationLink("", destination: Text("앱"))}
+                        }
                         
                     }
                     Section() {
@@ -319,7 +301,7 @@ struct ContentView: View {
                                 .background(.gray)
                                 .cornerRadius(5)
                             Text("개발자")
-                            NavigationLink("", destination: Text("개발자"))}
+                        }
                         
                     }
                 }
